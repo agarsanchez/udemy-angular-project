dd75f94258031a1bee3f2b450058292f41f4f305
Alberto Garcia <albertogarcia1984@gmail.com>
2018-01-11 20:32:13 +0000

    Add basic form cleaning

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -11,16 +11,23 @@ export class ShoppingEditComponent implements OnInit {
   @Output() ingredientAdded: EventEmitter<Ingredient> = new EventEmitter();
 
   @ViewChild('nameInput') nameInputRef: ElementRef;
+
   @ViewChild('amountInput') amountInputRef: ElementRef;
 
   constructor() {
   }
+
+  clearForm = function () {
+    this.nameInputRef.nativeElement.value = '';
+    this.amountInputRef.nativeElement.value = '';
+  };
 
   addIngredient() {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const ingredient = new Ingredient(ingName, ingAmount);
     this.ingredientAdded.emit(ingredient);
+    this.clearForm();
   }
 
   ngOnInit() {
